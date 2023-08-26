f1e37ef440c127696011765c2ef96e13ad44ff47
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2023-08-26 20:04:44 -0300

    feat: add libs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import Formulario from './componentes/Formulario';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
 function App() {
   return (
-    <Formulario />
+    <BrowserRouter>
+      <RecoilRoot>
+        <Routes>
+          <Route path="/" element={Formulario} />
+        </Routes>
+      </RecoilRoot>
+    </BrowserRouter>
   );
 }
 
