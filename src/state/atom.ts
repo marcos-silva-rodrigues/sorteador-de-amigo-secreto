2c7b7c34e0e734d2dbb78157ce89bed64a09cdfb
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2023-08-26 20:09:45 -0300

    feat: add state and hook

---
+++ src/state/atom.ts
@@ -0,0 +1,6 @@
+import { atom } from 'recoil';
+
+export const listaDeParticipantesState = atom<string[]>({
+    key: 'listaDeParticipantesState',
+    default: []
+});
---
+++ src/state/hooks/useAdicionarParticipante.ts
@@ -0,0 +1,10 @@
+import { useSetRecoilState } from 'recoil';
+import { listaDeParticipantesState } from '../atom';
+
+export const useAdicionarParticipante = () => {
+    const setLista = useSetRecoilState(listaDeParticipantesState);
+
+    return (nomeDoParticipante: string) => {
+        return setLista(listaAtual => [...listaAtual, nomeDoParticipante])
+    }
+}
