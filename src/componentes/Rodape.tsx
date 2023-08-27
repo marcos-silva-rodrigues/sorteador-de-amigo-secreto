87153e096039b7df8711549eca71c4fdff74d1e9
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2023-08-26 21:10:43 -0300

    test: Rodape

---
+++ src/componentes/Rodape.tsx
@@ -0,0 +1,15 @@
+import { useListaDeParticipantes } from "../state/hooks/useListaDeParticipantes";
+
+const Rodape = () => {
+    const participantes = useListaDeParticipantes();
+
+    return (
+        <footer>
+            <button disabled={participantes.length < 3}>
+                Iniciar brincadeira
+            </button>
+        </footer>
+    )
+}
+
+export default Rodape;
